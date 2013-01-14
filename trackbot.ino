8b7b631051ced0126c6b5a444d5e0d825e4927ed
Romilly Cocking <romilly@quick2wire.com>
2013-01-14 17:13:32 +0000

    setup pins, flash LED

--- trackbot.ino
+++ trackbot.ino
@@ -1,1 +1,57 @@
-// hello mum
+/* CC Share Alike 3.0 quick2wire.com 2013
+ */
+// driver for trackbot
+// for Arduino mini pro pinouts see http://arduino.cc/en/uploads/Main/Arduino-Pro-Mini-schematic.pdf
+// for motor control pin names see https://content.solarbotics.com/products/datasheets/solarbotics_l298_compact_motor_driver_kit.pdf
+
+// Right (from front of robot) side motor pins 
+const int L4  = 3;    // yellow, d3
+const int L3  = 4;    // orange, d4
+const int E34 = 5     // blue, motor PWM 
+
+// Left side motor pins
+const int L2  = 7;    // yellow, d7
+const int L1  = 8;    // orange, d8
+const int E12 = 9;    // blue, motor PWM
+
+const int onboardLED = 13;
+
+void flashOnboardLED() {
+  digitalWrite(onboardLED, HIGH);
+  delay(800);
+  digitalWrite(onboardLED, LOW);
+}
+
+void flashForStartup() {
+  pinMode(onboardLED, OUTPUT);
+  flashOnboardLED();
+  delay(200);
+  flashOnboardLED();
+}
+
+void setup() {
+  // digital pins
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L3, OUTPUT);
+  pinMode(L4, OUTPUT);
+  
+  flashForStartup();
+}
+
+void loop() {
+  
+  
+  // left forward
+  // wait 1 sec
+  // left backward
+  // wait 1 sec
+  // right forward
+  // wait 1 sec
+  // right backward
+  // wait 1 sec
+}
+
+
+
+
